db8436a2cc7399887cb7d13bb0727b53a452a383
Scott Maxwell <scottwmwork@gmail.com>
2019-09-23 15:01:45 -0700

    Added notebook for project

---
+++ notebooks/README.md
@@ -0,0 +1,4 @@
+#predict movie revenue
+
+Plotly Dash Web App to predict movie revenue based on movie kaggle dataset.
+It's my demo for Lambda School DS7.
--- run.py
+++ run.py
@@ -26,7 +26,7 @@ sticky (string, optional): Stick the navbar to the top or the bottom of the view
 """
 
 navbar = dbc.NavbarSimple(
-    brand='YOUR APP NAME',
+    brand='Movie Revenue',
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
@@ -44,11 +44,10 @@ footer = dbc.Container(
         dbc.Col(
             html.P(
                 [
-                    html.Span('Your Name', className='mr-2'), 
-                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
-                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
-                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
-                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/<you>'), 
+                    html.Span('Scott Maxwell ', className='mr-2'),  
+                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/scottwmwork/predict-movie-revenue'), 
+                    html.A(html.I(className='fab fa-linkedin mr-1'), href='www.linkedin.com/in/scott-maxwell-415171142'), 
+                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/Scott_W_Maxwell'), 
                 ], 
                 className='lead'
             )
